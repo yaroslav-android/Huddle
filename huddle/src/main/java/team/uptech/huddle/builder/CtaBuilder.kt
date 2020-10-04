9784f004c97b96db7de6ff20b419414ead1a9679
Rudem <ernesoglu@gmail.com>
2020-10-05 01:29:49 +0300

    - cta color naming simplification

--- huddle/src/main/java/team/uptech/huddle/builder/CtaBuilder.kt
+++ huddle/src/main/java/team/uptech/huddle/builder/CtaBuilder.kt
@@ -11,9 +11,9 @@ import team.uptech.huddle.util.ElementMarker
  * Allows to build a dialog with single or dual buttons
  *
  * @property text the text for the cta
- * @property textColorRes the color resource of the cta text
- * @property backgroundColorRes the color resource of the cta background
- * @property rippleColorRes the color resource of the cta ripple
+ * @property textColor the color resource of the cta text
+ * @property backgroundColor the color resource of the cta background
+ * @property rippleColor the color resource of the cta ripple
  * @property onClick the lambda for the cta clicks
  */
 @ElementMarker
@@ -21,9 +21,9 @@ class CtaBuilder {
   var onClick: ((dialog: Huddle) -> Unit)? = null
 
   var text: String = ""
-  var textColorRes: Int = DEFAULT_COLOR
-  var backgroundColorRes: Int = DEFAULT_COLOR
-  var rippleColorRes: Int = DEFAULT_COLOR
+  var textColor: Int = DEFAULT_COLOR
+  var backgroundColor: Int = DEFAULT_COLOR
+  var rippleColor: Int = DEFAULT_COLOR
 
   fun onClick(block: (dialog: Huddle) -> Unit) {
     onClick = block
